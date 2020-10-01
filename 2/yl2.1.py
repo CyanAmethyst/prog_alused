1f714fa2724bb2231684795e9632430f09157c5d
jannejaago <janneaps@gmail.com>
2020-10-01 11:47:15 +0300

    first commit

---
+++ 2/yl2.1.py
@@ -0,0 +1,5 @@
+temp=float(input("Sisesta õhutemperatuur: "))
+if(temp<=4):
+    print("On jäätumise oht")
+else:
+    print("Ei ole jäätumise oht")
---
+++ 2/yl2.2.py
@@ -0,0 +1,7 @@
+nimi=input("Sisestage Leedu perekonnanimi: ")
+if(nimi[-2]=="ne"):
+    print("Abielus")
+elif(nimi[-2]=="te"):
+    print("Vallaline")
+elif(nimi[-1]=="e"):
+    print("Määramata")
