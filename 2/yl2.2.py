5d20d7b0f8f08cb61e6ef4e86903f45f782b854f
jannejaago <janneaps@gmail.com>
2020-10-08 10:15:30 +0300

    first commit

--- 2/yl2.2.py
+++ 2/yl2.2.py
@@ -1,7 +1,9 @@
 nimi=input("Sisestage Leedu perekonnanimi: ")
-if(nimi[-2]=="ne"):
+if(nimi[-2:]=="ne"):
     print("Abielus")
-elif(nimi[-2]=="te"):
+elif(nimi[-2:]=="te"):
     print("Vallaline")
 elif(nimi[-1]=="e"):
     print("Määramata")
+else:
+    print("Ei ole Leedu perenimi")
